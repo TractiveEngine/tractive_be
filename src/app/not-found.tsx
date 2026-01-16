c77d0cbc3000a924658972163eacb1613ad1cbf9
Zeeson <olabrazanislam@gmail.com>
2026-01-16 07:51:55 -0500

    update next

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1 style={{ marginBottom: 8 }}>Page not found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
